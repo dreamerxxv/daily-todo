fa6f396b22d1f00a51d58280cf2f56dc89634b08
dreamerxxv <successentrepren@gmail.com>
2025-02-21 21:17:47 +0700

    gitignore n app update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,7 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 // export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 function App() {
   return (
     <>
