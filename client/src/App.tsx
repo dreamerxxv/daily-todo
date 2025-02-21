dc5957bd1f6d54d85e9a7e5320734e178930a7b8
dreamerxxv <successentrepren@gmail.com>
2025-02-21 21:02:47 +0700

    App update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,16 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 // export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 function App() {
   return (
     <>
-      <Navbar />
-      <TodoForm />
-      <TodoList />
+      <div>
+        <Navbar />
+        <TodoForm />
+        <TodoList />
+      </div>
     </>
   );
 }
